4b9d01080bb3c78ab2644dcbc2e775d9e7889120
“mingpeng.mao” <mingpeng.mao@t2mobile.com>
2025-03-26 17:12:18 +0800

    [FPS-1309][OLED] Fix screen display related issues[3/3]
    Issues:
    1.The screen is yellow and blanking.
    2.60hz is very stuck.
    RootCause:
    1.dbv cmds were writed not in a whole pac
        and porch modified.
    2.After setting frame rate to 60hz, dbv cmds update.
    Solution:
    1.Send the dbv cmds as a whloe package and back porch.
    2.After dbv cmds updating, set frame rate to 60hz again.
    Test Steps:
    follow jira.
    
    Change-Id: I8eb82fd0b1b41d594f75bd35282bd50dac6830e0

--- qcom/opensource/display-drivers/msm/dsi/dsi_panel.h
+++ qcom/opensource/display-drivers/msm/dsi/dsi_panel.h
@@ -278,6 +278,10 @@ struct dsi_panel {
 	enum dsi_panel_physical_type panel_type;
 
 	struct dsi_panel_ops panel_ops;
+
+#if defined(CONFIG_ARCH_FPSPRING)
+	int current_bl;
+#endif
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
